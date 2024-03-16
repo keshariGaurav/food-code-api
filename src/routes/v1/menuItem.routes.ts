4531efc218bdfc4d6bc65f00fc5857e309270a18
Ayushi <khandeliaayushi08@gmail.com>
2024-03-16 15:14:20 +0530

    used mutler to store image in mongodb

--- src/routes/v1/menuItem.routes.ts
+++ src/routes/v1/menuItem.routes.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import { v1MenuItemControllers } from '../../controllers/index';
 import { v1AuthCafeControllers } from '../../controllers/index';
+import multer from 'multer';
+
+const upload = multer();
 
 const router = express.Router();
 
 router.get('/', v1MenuItemControllers.getAll);
 router.get('/:id', v1MenuItemControllers.getOne);
-router.post('/', v1MenuItemControllers.create);
+router.post('/', upload.single('image') , v1MenuItemControllers.create);
 router.patch(
     '/:id',
     v1AuthCafeControllers.protect,
