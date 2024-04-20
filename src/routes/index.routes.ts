94ed6fab0d47dc848ad9c727418be179710c22ea
Ayushi <khandeliaayushi08@gmail.com>
2024-04-20 18:09:51 +0530

    fixed semicolon issue

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -11,6 +11,6 @@ rootRouter.use('/v1/cafe', v1CafeRouter);
 rootRouter.use('/v1/menus', v1MenuItemRouter);
 rootRouter.use('/v1/category', v1CategoryItemRouter);
 rootRouter.use('/v1/order', v1OrderRouter);
-rootRouter.use('/v1/diner' , v1DinerRouter)
+rootRouter.use('/v1/diner' , v1DinerRouter);
 
 export { rootRouter };
