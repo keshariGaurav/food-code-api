de175b81f1315671cc92ba5290ead791e77b9c62
gaurav keshari <kesharigaurav97@gmail.com>
2024-05-18 20:21:53 +0530

    feat(diner-login): Create login api for diner
    1) Create login route for otp login through mail.
    2) Implement google oauth.

--- src/routes/v1/diner.routes.ts
+++ src/routes/v1/diner.routes.ts
@@ -1,11 +1,25 @@
 import express from 'express';
+import passport from 'passport';
 import { v1DinerControllers } from '../../controllers/index';
-import { v1AuthCafeControllers } from '../../controllers/index';
+import { v1AuthDinerControllers } from '../../controllers/index';
 
 const router = express.Router();
 
 router.get('/', v1DinerControllers.getAll);
 router.get('/:id', v1DinerControllers.getOne);
 router.post('/',  v1DinerControllers.create);
-
+router.get(
+    '/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+router.get(
+    '/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    v1AuthDinerControllers.authCallback
+);
+router.post(
+    '/login',
+    v1AuthDinerControllers.sendLoginOtp
+);
+router.post('/verify-login', v1AuthDinerControllers.login);
 export { router };
