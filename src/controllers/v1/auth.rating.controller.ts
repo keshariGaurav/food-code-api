7edbf1fbff9529d70b6191f5fffea8a48e888132
Ayushi <khandeliaayushi08@gmail.com>
2024-06-30 13:14:48 +0530

    moved diner auth from rating to diner

--- src/controllers/v1/auth.rating.controller.ts
+++ src/controllers/v1/auth.rating.controller.ts
@@ -6,53 +6,7 @@ import jwt, { TokenExpiredError } from 'jsonwebtoken';
 import diner,{IDiner} from '../../models/diner.model';
 import Rating from '../../models/rating.model';
 
-export const authMiddleware = catchAsync(
-    async (req: Request, res: Response, next: NextFunction) => {
-        let token: string | undefined;
-        if (
-            req.headers.authorization &&
-            req.headers.authorization.startsWith('Bearer')
-        ) {
-            token = req.headers.authorization.split(' ')[1];
-        } else if (req.cookies.jwt) {
-            token = req.cookies.jwt;
-        }
 
-        if (!token) {
-            return next(
-                new AppError(
-                    'You are not logged in! Please login to get access.',
-                    401
-                )
-            );
-        }
-        try {
-            const decoded = await new Promise<{ id: string; iat: number }>(
-                (resolve, reject) => {
-                    jwt.verify(
-                        token!,
-                        process.env.JWT_SECRET,
-                        (error, decoded) => {
-                            if (error) {
-                                reject(error);
-                            } else {
-                                resolve(decoded as { id: string; iat: number });
-                            }
-                        }
-                    );
-                }
-            );
-            const user = await diner.findById(decoded.id);
-            if (!user) {
-                return next(new AppError('The user no longer exists', 401));
-            }
-            req.user = user;
-            next();
-        } catch (error) {
-            return next(new AppError('Invalid token', 401));
-        }
-    }
-);
 
 export const verifyRating = catchAsync(
     async (req: Request, res: Response, next: NextFunction) => {
--- src/routes/v1/rating.routes.ts
+++ src/routes/v1/rating.routes.ts
@@ -1,7 +1,7 @@
 import express from 'express';
-import { v1AuthRatingControllers , v1RatingControllers } from '../../controllers';
+import { v1AuthDinerControllers, v1AuthRatingControllers , v1DinerControllers, v1RatingControllers } from '../../controllers';
 const router = express.Router();
 
 router.get('/' , v1RatingControllers.getAll);
-router.post('/' , v1AuthRatingControllers.authMiddleware, v1AuthRatingControllers.verifyRating, v1RatingControllers.create);
+router.post('/' , v1AuthDinerControllers.protect, v1AuthRatingControllers.verifyRating, v1RatingControllers.create);
 export { router };
