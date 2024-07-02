89f91b8de369e1622a9fa8d1aba46b30ea360493
gaurav keshari <kesharigaurav97@gmail.com>
2024-07-02 12:23:33 +0530

    run lint fix

--- src/middleware/cacheMiddleware.ts
+++ src/middleware/cacheMiddleware.ts
@@ -8,28 +8,28 @@ interface CustomResponse extends Response {
 
 const cacheMiddleware =
     (tags: string[]) =>
-    async (req: Request, res: CustomResponse, next: NextFunction) => {
-        const key = generateCacheKey(req);
+        async (req: Request, res: CustomResponse, next: NextFunction) => {
+            const key = generateCacheKey(req);
 
-        try {
-            const cachedData = await redisClient.get(key);
+            try {
+                const cachedData = await redisClient.get(key);
 
-            if (cachedData) {
-                return res.send(JSON.parse(cachedData));
+                if (cachedData) {
+                    return res.send(JSON.parse(cachedData));
+                }
+
+                res.sendResponse = res.send.bind(res);
+                res.send =  (body) => {
+                    redisClient.set(key, JSON.stringify(body), 'EX', 3600);
+                    tagCacheEntry(key, tags);
+                    return res.sendResponse!(body);
+                };
+
+                next();
+            } catch (error) {
+                console.error('Redis error:', error);
+                next();
             }
-
-            res.sendResponse = res.send.bind(res);
-            res.send =  (body) => {
-                redisClient.set(key, JSON.stringify(body), 'EX', 3600);
-                tagCacheEntry(key, tags);
-                return res.sendResponse!(body);
-            };
-
-            next();
-        } catch (error) {
-            console.error('Redis error:', error);
-            next();
-        }
-    };
+        };
 
 export default cacheMiddleware;
